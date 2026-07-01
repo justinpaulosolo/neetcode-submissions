190e26edc04d9d65c306cd48687de2ecce4263d3
Justin Solo <justin@justinsolo.com>
2026-06-30 17:39:41 -0700

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) {
+        Dictionary <string, List<string>> dict = new();
+
+        foreach(var s in strs)
+        {
+            var count = new int[26];
+            foreach(var c in s)
+            {
+                count[c - 'a'] += 1;
+            }
+
+            string result = string.Join(",", count);
+
+            if(dict.TryGetValue(result, out List<string> value))
+            {
+                value.Add(s);
+            } else
+            {
+                dict.Add(result, new List<string>{s});
+            }
+        }
+        return new List<List<string>>(dict.Values);
+    }
+}

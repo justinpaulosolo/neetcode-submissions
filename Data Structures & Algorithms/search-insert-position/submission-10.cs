60333d3729e9c680510163ee3b10411ebbcbe3ad
Justin Solo <justin@justinsolo.com>
2026-07-15 02:39:51 -0700

    Add: search-insert-position - submission-10

---
+++ Data Structures & Algorithms/search-insert-position/submission-10.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int SearchInsert(int[] nums, int target) {
+        int left = 0, right = nums.Length-1;
+        int mid = 0;
+
+        while(left <= right)
+        {
+            mid = (left + right) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+                
+        }
+ 
+        return left;
+    }
+}
